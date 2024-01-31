f6be8b19f25f2933aef631dbd2e9b0d8220ebc75
Enes Öztürk <enes.ozturk@unikie.com>
2024-01-31 18:20:14 +0200

    fail_on_error is added building phase

--- build.rs
+++ build.rs
@@ -4,11 +4,12 @@ use vergen_gix::{
 };
 
 pub fn main() -> Result<()> {
-    Emitter::default()
+    Emitter::new()
         .add_instructions(&BuildBuilder::all_build()?)?
         .add_instructions(&CargoBuilder::all_cargo()?)?
         .add_instructions(&GixBuilder::all_git()?)?
         .add_instructions(&RustcBuilder::all_rustc()?)?
         .add_instructions(&SysinfoBuilder::all_sysinfo()?)?
+        .fail_on_error()
         .emit()
 }
